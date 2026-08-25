84557728810f41fd9d35aa6ae8bd26cdc4b0de76
TWMKyle <ktrrsnf3@gmail.com>
2026-08-26 01:27:05 +0800

    Update TWMWeb.py

--- TWMWeb.py
+++ TWMWeb.py
@@ -11,8 +11,8 @@ from streamlit_gsheets import GSheetsConnection, gsheets_connection
 
 wedding_photos = [
     "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_3815.jpg",
-    "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_8287 2.JPG",
-    "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_8288 2.JPG"
+    "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_82872.JPG",
+    "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_82882.JPG"
 ]
 
 img_src1 = "https://raw.githubusercontent.com/TWMKyle/twm/main/a38d642b-d7f2-4989-91bc-13fd2048683c.jpg"
