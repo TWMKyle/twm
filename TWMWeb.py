6596175c6c507d08ed19d319c77b8becdc2f216b
TWMKyle <ktrrsnf3@gmail.com>
2026-08-26 01:18:05 +0800

    Update TWMWeb.py

--- TWMWeb.py
+++ TWMWeb.py
@@ -13,6 +13,15 @@ from streamlit_gsheets import GSheetsConnection, gsheets_connection
 img_src1 = "https://raw.githubusercontent.com/TWMKyle/twm/main/a38d642b-d7f2-4989-91bc-13fd2048683c.jpg"
 img_srcp = "https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_3815.jpg"
 
+html_photo_stack = '<div class="photo-scroll-container">'
+for url in wedding_photos:
+    html_photo_stack += f'<img src="{"https://raw.githubusercontent.com/TWMKyle/twm/main/IMG_3815.jpg"}" class="portrait-stack-img" />'
+html_photo_stack += '</div>'
+
+# 2. Inject the scroll stack container right into the side panel
+st.sidebar.markdown(html_photo_stack, unsafe_allow_html=True)
+
+
 # 2. Inject styling to target the ENTIRE Streamlit app frame
 st.markdown(
     f"""
